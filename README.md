0f24663287fc271af21b209ca9f614381db1fe37
Samuele Germiniani <samuele.germiniani@univr.it>
2023-04-05 17:41:56 +0200

    fixed broken faisal

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# gmBenchmark
+
+#Requirements
+
+- Modelsim on the $PATH (vsim, vlog, vlib)
+
+Run the "runFaisal.sh" script in the "faisal" directory to simulate the faults and generate the vcd files. 
+
--- ibex_id_stage/harm/faisal/runFaisel.sh
+++ ibex_id_stage/harm/faisal/runFaisel.sh
@@ -1,1 +1,1 @@
-./faisal --clk clk -c faisal_conf.xml --vcd golden.vcd --out output/ --oo --vcd_r --max_threads 16
+./faisal --clk clk_i -c faisal_conf.xml --vcd golden.vcd --out output/ --oo --vcd_r --max_threads 16
